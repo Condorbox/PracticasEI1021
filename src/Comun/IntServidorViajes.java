0ac337fbe8ed5d64e4c90b871a2c805ac9e013b1
condorbox <minecraft13122001@gmail.com>
2022-10-18 11:08:56 +0200

    Servidor Viajes Interface

--- src/Comun/IntServidorViajes.java
+++ src/Comun/IntServidorViajes.java
@@ -1,4 +1,17 @@
 package Comun;
 
-public interface IntServidorViajes {
+import Gestor.Viaje;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IntServidorViajes extends Remote {
+    void guardarDatos() throws RemoteException;
+    JSONArray consultaViajes(String origen) throws RemoteException;
+    JSONObject reservaViaje(String codviaje, String codcli) throws RemoteException;
+    JSONObject anulaReserva(String codviaje, String codcli) throws RemoteException;
+    JSONObject ofertaViaje(String codcli, String origen, String destino, String fecha, long precio, long numplazas) throws RemoteException;
+    JSONObject borraViaje(String codviaje, String codcli) throws RemoteException;
 }
